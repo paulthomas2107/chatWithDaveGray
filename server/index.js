be3ca3b55c64238ca7741827bcd6aedbd6497cd4
Paul Thomas <paulthomas2107@gmail.com>
2023-10-25 13:43:52 +0100

    ddd

--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
-const ws = require('ws');
+import { ws } from 'ws';
+
 const server = new ws.Server({ port: '3000' });
 
 server.on('connection', (socket) => {
   socket.on('message', (message) => {
-    console.log(message);
+    const b = Buffer.from(message);
+    console.log(message, b.toString());
     socket.send(`${message}`);
   });
 });
